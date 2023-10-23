74a59d01be81e401d1d6808167648f631b4419e3
Pierre <cpierredavis@gmail.com>
2023-10-23 14:16:16 +0200

    Commit made from VS Code Azure Static Web Apps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: process.env.NODE_ENV === 'development' ? false : true,
   devtools: {
     enabled: true,
     timeline: {
