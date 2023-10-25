6b01a1b3a0b13bd5b524a2b2d6c22187029c7b04
Pierre <cpierredavis@gmail.com>
2023-10-25 17:26:24 +0200

    add to app.vue file asd well

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script>
+useHead({
+  title: "Star Wars - HomePage",
+  meta: [
+    { name: "description", content: "A site dedicated to Starwars" },
+    { property: "og:title", content: "Star Wars" },
+    { property: "og:description", content: "A site dedicated to Starwars" },
+    { property: "og:image", content: "/img/yoda.jpeg" },
+  ],
+});
+</script>
 <template>
   <div>
     <NuxtPage />
